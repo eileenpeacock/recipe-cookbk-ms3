37c4a2357b44baad4985b5e7d86d72e5337d5765
eileenpeacock <ejpeacock@hotmail.com>
2019-10-19 22:39:30 +0100

    updated app.py with mongo

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+# import modules
 import os
 from flask import Flask
+from flask import render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+# set app environment variables
+app.config["MONGO_DBNAME"] = 'recipesDB'
+app.config["MONGO_URI"] = 'os.environ.get('MONGO_URI')'
+
+mongo = PyMongo(app)
+
 @app.route('/')
 def hello():
-    return 'Hello World test 2'
+    return 'Hello World ...test 2'
 
 # Global settings
 # if __name__ == '__main__':      
